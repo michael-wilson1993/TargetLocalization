283672af466eabe68823902de5f0e47af5382abc
Austin Kothig <kothiga2112@gmail.com>
2017-01-21 22:07:55 -0700

    Detecting Balls. XY coordinats are known. Drawing shapes around ball like objects from threshold

--- redColor/redColor.cpp
+++ redColor/redColor.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 using namespace std;
 
+RNG rng(12345);
+
 int main( int argc, char** argv )
 {
-  VideoCapture cap(0); //capture the video from webcam
+  // capture the video from webcam
+  VideoCapture cap(0); 
 
-  if ( !cap.isOpened() )  // if not success, exit program
+  // if not success, exit program
+  if ( !cap.isOpened() )  
     {
       cout << "Cannot open the web cam" << endl;
       return -1;
     }
 
-  
-  namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+  // create a window called "Control"
+  namedWindow("Control", CV_WINDOW_AUTOSIZE); 
 
-  // presets for my testing (very accurate atleast for my light) 
+  // presets for my testing
+  // (very accurate atleast for my light) 
   int iLowH = 0;
   int iHighH = 9;
 
@@ -28,48 +35,56 @@ int main( int argc, char** argv )
   int iLowV = 89;
   int iHighV = 255;
 
-  //Create trackbars in "Control" window
+  int thresh = 100;
+  int max_thresh = 255;
+
+  // Create trackbars in "Control" window
   createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
   createTrackbar("HighH", "Control", &iHighH, 179);
 
   createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
   createTrackbar("HighS", "Control", &iHighS, 255);
 
-  createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+  createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
   createTrackbar("HighV", "Control", &iHighV, 255);
+
+  // For control of Circle Threshold
+  createTrackbar( " Threshold", "Control", &thresh, max_thresh);
 
   int iLastX = -1; 
   int iLastY = -1;
 
-  //Capture a temporary image from the camera
+  // Capture a temporary image from the camera
   Mat imgTmp;
   cap.read(imgTmp); 
 
-  //Create a black image with the size as the camera output
-  Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );;
- 
+  // Create a black image with the size as the camera output
+  Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );
 
+  // main loop
   while (true)
     {
+      // read a new frame from video
       Mat imgOriginal;
+      bool bSuccess = cap.read(imgOriginal); 
 
-      bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-
-
-
-      if (!bSuccess) //if not success, break loop
+      // if not success, break loop
+      if (!bSuccess) 
         {
 	  cout << "Cannot read a frame from video stream" << endl;
 	  break;
         }
 
+      
       Mat imgHSV;
 
-      cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+      //Convert the captured frame from BGR to HSV
+      cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); 
  
       Mat imgThresholded;
 
-      inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+      //Threshold the image
+      inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); 
       
       //morphological opening (removes small objects from the foreground)
       erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
@@ -79,40 +94,88 @@ int main( int argc, char** argv )
       dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
       erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
+
+      //#####################################//
+      //                                     //
+      //          Draw on Threshold          //
+      //                                     //
+      //#####################################//
+      vector<vector<Point> > contours;
+      vector<Vec4i> hierarchy;
+      
+      // Detect edges using Threshold
+      threshold( imgThresholded, imgThresholded, thresh, 255, THRESH_BINARY );
+
+      // Find contours
+      findContours( imgThresholded, contours, hierarchy, CV_RETR_TREE,
+		    CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+      // Approximate contours to polygons + get bounding rects and circles
+      vector<vector<Point> > contours_poly( contours.size() );
+      vector<Rect> boundRect( contours.size() );
+      vector<Point2f>center( contours.size() );
+      vector<float>radius( contours.size() );
+
+      for( unsigned int i = 0; i < contours.size(); i++ )
+	{
+	  approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+	  boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+	  minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+	}
+
+      // Draw polygonal contour + bonding rects + circles
+      //Mat drawing = Mat::zeros( imgThresholded.size(), CV_8UC3 );
+      for( unsigned int i = 0; i< contours.size(); i++ )
+	{
+	  Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+	  drawContours( imgThresholded, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );	  
+	  rectangle( imgThresholded, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+	  circle( imgThresholded, center[i], (int)radius[i], color, 2, 8, 0 );
+	}
+      
       //Calculate the moments of the thresholded image
       Moments oMoments = moments(imgThresholded);
 
       double dM01 = oMoments.m01;
       double dM10 = oMoments.m10;
       double dArea = oMoments.m00;
-
-      // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+      
+      // if the area <= 10000, I consider that the there are no object
+      // in the image and it's because of the noise, the area is not zero 
       if (dArea > 10000)
 	{
 	  //calculate the position of the ball
 	  int posX = dM10 / dArea;
-	  int posY = dM01 / dArea;        
-        
+	  int posY = dM01 / dArea;
+	  
 	  if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
 	    {
 	      //Draw a red line from the previous point to the current point
 	      line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0,0,255), 2);
 	    }
 
+	  //#############################
+	  // x y positions are here
+	  //#############################
 	  iLastX = posX;
 	  iLastY = posY;
 	}
-      /*
-      // draw box around circular objects
-      imgThresholded 
-      */
       
+      // print to console
+      cout << "X : " << iLastX << " Y : " << iLastY << "\n";     
+
+
+      //#####################################//
+      //                                     //
+      //               Display               //
+      //                                     //
+      //#####################################//
       imshow("Thresholded Image", imgThresholded); //show the thresholded image
+      imgOriginal += imgLines;                     //transpose imglines
+      imshow("Original", imgOriginal);             //show the original image 
 
-      imgOriginal = imgOriginal + imgLines;
-      imshow("Original", imgOriginal); //show the original image
-
-      if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+      //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+      if (waitKey(30) == 27) 
 	{
 	  cout << "esc key is pressed by user" << endl;
 	  break; 
@@ -121,64 +184,3 @@ int main( int argc, char** argv )
 
   return 0;
 }
-
-
-
-/*
-#include "opencv2/core.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/videoio.hpp"
-#include <iostream>
-
-using namespace cv;
-using namespace std;
-
-void drawText(Mat & image);
-
-int main()
-{
-    cout << "Built with OpenCV " << CV_VERSION << endl;
-    Mat image;
-    VideoCapture capture;
-    capture.open(1);
-    if(capture.isOpened())
-    {
-        cout << "Capture is opened" << endl;
-        for(;;)
-        {
-            capture >> image;
-            if(image.empty())
-                break;
-            drawText(image);
-
-	    // detect ball here
-	    image.cvtColor();
-	    
-
-	    
-            imshow("Sample", image);
-            if(waitKey(10) >= 0)
-                break;
-        }
-    }
-    else
-    {
-        cout << "No capture" << endl;
-        image = Mat::zeros(480, 640, CV_8UC1);
-        drawText(image);
-        imshow("Sample", image);
-        waitKey(0);
-    }
-    return 0;
-}
-
-void drawText(Mat & image)
-{
-    putText(image, "Hello OpenCV",
-            Point(20, 50),
-            FONT_HERSHEY_COMPLEX, 1, // font face and scale
-            Scalar(255, 255, 255), // white
-            1, LINE_AA); // line thickness and type
-}
-*/
